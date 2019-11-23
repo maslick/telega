0a5a04022a9fa6764839cf2fbe3d53b86ff90ba1
maslick <pavel.masloff@gmail.com>
2019-11-23 01:58:56 +0100

    to spaces

--- telega_test.go
+++ telega_test.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"github.com/stretchr/testify/assert"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+    "bytes"
+    "encoding/json"
+    "github.com/stretchr/testify/assert"
+    "net/http"
+    "net/http/httptest"
+    "testing"
 )
 
 type Mock struct{}
 
 func (m *Mock) SendTelegramMessage(token, chat, message string) ([]byte, error) {
-	return []byte(message), nil
+    return []byte(message), nil
 }
 
 func TestHealthEndpoint(t *testing.T) {
-	server := RestController{&Mock{}}
-	req, _ := http.NewRequest("GET", "/health", nil)
+    server := RestController{&Mock{}}
+    req, _ := http.NewRequest("GET", "/health", nil)
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(server.HealthHandler)
-	handler.ServeHTTP(rr, req)
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(server.HealthHandler)
+    handler.ServeHTTP(rr, req)
 
-	assert.Equal(t, http.StatusOK, rr.Code)
-	assert.Equal(t, "text/plain", rr.Header().Get("Content-Type"))
-	assert.Equal(t, "UP", rr.Body.String())
+    assert.Equal(t, http.StatusOK, rr.Code)
+    assert.Equal(t, "text/plain", rr.Header().Get("Content-Type"))
+    assert.Equal(t, "UP", rr.Body.String())
 }
 
 func TestSendEndpoint(t *testing.T) {
-	server := RestController{&Mock{}}
-	body := new(bytes.Buffer)
-	_ = json.NewEncoder(body).Encode(Message{"chat", "hello world"})
-	req, _ := http.NewRequest("POST", "/send", body)
+    server := RestController{&Mock{}}
+    body := new(bytes.Buffer)
+    _ = json.NewEncoder(body).Encode(Message{"chat", "hello world"})
+    req, _ := http.NewRequest("POST", "/send", body)
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(server.SendHandler)
-	handler.ServeHTTP(rr, req)
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(server.SendHandler)
+    handler.ServeHTTP(rr, req)
 
-	assert.Equal(t, http.StatusOK, rr.Code)
-	assert.Equal(t, "application/json", rr.Header().Get("Content-Type"))
-	assert.Equal(t, "hello world", rr.Body.String())
+    assert.Equal(t, http.StatusOK, rr.Code)
+    assert.Equal(t, "application/json", rr.Header().Get("Content-Type"))
+    assert.Equal(t, "hello world", rr.Body.String())
 }
